a15af9cae92d1110459309da1e7621991749baac
Sammy Gransden <108677712+sammygee123@users.noreply.github.com>
2022-12-06 21:36:35 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const carouselImages =document.querySelector("carousel-images");
+const carouselButtons = document.querySelectorAll("carousel-button");
+const numberOfImages = document.querySelectorAll("carousel-images img").length;
+let imageIndex = 1;
+let translateX = 0;
+
+carouselButtons.forEach(button =>{
+button.addEventListener(click, event =>{
+    if (event.target.id === "previous"){
+        if (imageIndex !== 1) {
+            imageIndex--;
+            translateX =+ 300;
+        }
+    }else {
+    if (imageIndex !== numberOfImages){
+        imageIndex++;
+        translateX -= 300;
+    }
+}
+    carouselImages.style.transform = "translateX(${translateX}px)";
+});
+});
